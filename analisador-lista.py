136d6da8295f229d0b6dd6fb0ad9366a043faabc
Gabriel Gomes Fernandes <130801640+ggfernandess@users.noreply.github.com>
2026-04-07 14:23:32 -0300

    Add files via upload

---
+++ analisador-lista.py
@@ -0,0 +1,15 @@
+# %%
+idades = [28, 42, 43, 35, 39, 28, 38, 42]
+print("Soma das idades:",sum(idades))
+
+print("Qtde idades:", len(idades))
+
+print("Media das idades:",sum(idades)/len(idades))
+
+print("Menor idade:", min(idades))
+
+print("Maior idade:", max(idades))
+
+"""
+Código feito pra praticar listas, onde foi feito usando SUM,LEN,MIN E MAX.
+"""
